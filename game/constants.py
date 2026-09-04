d32e6f007d6c72d2b5d87d6e3cdde0b5f1e35a47
CaesarOctavium <octavigavalda@gmail.com>
2026-09-04 02:36:11 +0200

    Add game constants and enums

---
+++ game/constants.py
@@ -0,0 +1,51 @@
+"""
+Game constants and enums
+"""
+
+# Gladiator types
+GLADIATOR_TYPE_RETARIUS = "retarius"      # Net fighter
+GLADIATOR_TYPE_MURMILLO = "murmillo"      # Fish fighter
+GLADIATOR_TYPE_THRAX = "thrax"            # Thracian
+
+# Enemy types
+ENEMY_TYPE_GLADIATOR = "gladiator"
+ENEMY_TYPE_BEAST = "beast"
+ENEMY_TYPE_WARRIOR = "warrior"
+ENEMY_TYPE_VETERAN = "veteran"
+
+# Weapon types
+WEAPON_SWORD = "sword"
+WEAPON_TRIDENT = "trident"
+WEAPON_NET = "net"
+WEAPON_SPEAR = "spear"
+
+# Armor piece types
+ARMOR_HELMET = "helmet"
+ARMOR_CHEST = "chest"
+ARMOR_LEGS = "legs"
+ARMOR_KNEES = "knees"
+ARMOR_BOOTS = "boots"
+
+# Armor quality levels
+ARMOR_QUALITY_PRIMITIVE = "primitive"
+ARMOR_QUALITY_LEATHER = "leather"
+ARMOR_QUALITY_METAL = "metal"
+ARMOR_QUALITY_LEGENDARY = "legendary"
+
+# Item rarity
+RARITY_COMMON = "common"
+RARITY_UNCOMMON = "uncommon"
+RARITY_RARE = "rare"
+RARITY_LEGENDARY = "legendary"
+
+# Gladiator status
+STATUS_ALIVE = "alive"
+STATUS_WOUNDED = "wounded"
+STATUS_DEAD = "dead"
+STATUS_RETIRED = "retired"
+
+# Combat outcomes
+OUTCOME_VICTORY = "victory"
+OUTCOME_DEFEAT = "defeat"
+OUTCOME_DEATH = "death"
+OUTCOME_FLED = "fled"
